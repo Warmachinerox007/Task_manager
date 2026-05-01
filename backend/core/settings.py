25f40da607cc86c92ebd86d6652719e7cb9d61e5
Warmachinerox007 <anirudhc131@gmail.com>
2026-05-01 16:52:49 +0530

    fix: correct __file__ syntax error

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from decouple import config
 import os
 
-BASE_DIR = Path(**file**).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 DEBUG = config('DEBUG', default=False, cast=bool)
